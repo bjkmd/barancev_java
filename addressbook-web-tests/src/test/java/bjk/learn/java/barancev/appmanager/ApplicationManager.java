112bd636de91fa02a306b0447a8a168a6b322e70
DENYS BOYKO <Denys_Boyko@epam.com>
2020-09-01 14:37:49 +0300

    Added browser selection logic

--- addressbook-web-tests/src/test/java/bjk/learn/java/barancev/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/bjk/learn/java/barancev/appmanager/ApplicationManager.java
@@ -4,20 +4,38 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.openqa.selenium.remote.BrowserType.*;
 
 public class ApplicationManager {
 
     protected WebDriver driver;
 
     NavigationHelper navigationHelper;
+    private final String browser;
+
+    public ApplicationManager(String browser) {
+
+        this.browser = browser;
+    }
 
     public NavigationHelper getNavigationHelper() {
         return navigationHelper;
     }
 
     public void init() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        switch (browser) {
+            case CHROME:
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case FIREFOX:
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+
+        }
         driver.get("http://localhost:8099/");
         driver.manage().window().setSize(new Dimension(1680, 991));
 
--- addressbook-web-tests/src/test/java/bjk/learn/java/barancev/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/bjk/learn/java/barancev/tests/TestBase.java
@@ -1,12 +1,13 @@
 package bjk.learn.java.barancev.tests;
 
 import bjk.learn.java.barancev.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeMethod
     public void setUp() {
