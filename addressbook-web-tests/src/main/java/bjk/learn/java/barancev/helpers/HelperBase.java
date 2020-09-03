2afc96b7b19dd1903e3c92b7eddf81984ad6ae83
DENYS BOYKO <Denys_Boyko@epam.com>
2020-09-03 11:51:21 +0300

    Added existing text presence check

--- addressbook-web-tests/src/main/java/bjk/learn/java/barancev/helpers/HelperBase.java
+++ addressbook-web-tests/src/main/java/bjk/learn/java/barancev/helpers/HelperBase.java
@@ -4,19 +4,26 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HelperBase {
-    protected WebDriver driver;
+  protected WebDriver driver;
 
-    public HelperBase(WebDriver driver) {
-        this.driver = driver;
-    }
+  public HelperBase(WebDriver driver) {
+    this.driver = driver;
+  }
 
-    public void click(By locator) {
-        driver.findElement(locator).click();
-    }
+  public void click(By locator) {
+    driver.findElement(locator).click();
+  }
 
-    public void type(By locator, String text) {
+  public void type(By locator, String text) {
+click(locator);
+    if (text != null) {
+      String existingText = driver.findElement(locator).getAttribute("value");
+      if (!text.equals(existingText)) {
+        driver.findElement(locator).clear();
         driver.findElement(locator).sendKeys(text);
+      }
     }
+  }
 
 
 }
